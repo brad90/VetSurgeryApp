40504067c0b15c42adb1d00f147ccef4d075e539
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-04-28 17:15:33 +0100

    Update and show id for a pet added

--- vet_surgery_controller.rb
+++ vet_surgery_controller.rb
@@ -12,22 +12,50 @@ get'/vet-surgery'do
 erb(:index)
 end
 
+#Show all the staff currently working
+get'/vet-surgery/staff'do
+  @staff = Staff.all
+erb(:staff_index)
+end
+
 #Get the information to create a new Animal
 get '/vet-surgery/new' do
   @staff = Staff.all()
   erb(:new)
 end
 
+#Get the information to create a new Staff
+get '/vet-surgery/new_staff' do
+  erb(:new_staff)
+end
 
 post '/vet-surgery' do
   @animals = Animal.new(params)
   @animals.save()
-# erb(:new)
+  redirect to '/vet-surgery'
+end
+
+post '/vet-surgery/staff' do
+  @staff = Staff.new(params)
+  @staff.save()
+  redirect to '/vet-surgery/staff'
+end
+
+get '/vet-surgery/:id/edit' do
+  @staff = Staff.all
+  @animal = Animal.find(params['id'])
+  erb(:edit)
 end
 
 
 #Show only one animals information in the vets
 get '/vet-surgery/:id' do
   @animal = Animal.find(params[:id])
+erb(:show)
+end
+
+#Show only one vets information in the vets
+get '/vet-surgery/staff/:id' do
+  @Staff = Staff.find(params[:id])
 erb(:show)
 end
