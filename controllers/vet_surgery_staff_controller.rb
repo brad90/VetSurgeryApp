cf398b32a4f4cf36a16c425d4d4ba7234fcadf69
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-04-29 11:27:34 +0100

    MVP complete, css stil to be finished

--- controllers/vet_surgery_staff_controller.rb
+++ controllers/vet_surgery_staff_controller.rb
@@ -29,11 +29,18 @@ get '/vet-surgery/staff/:id' do
 erb(:show_staff)
 end
 
+#Show only one vets information in the vets
+get '/vet-surgery/staff/:id/edit' do
+  @staff = Staff.find(params[:id])
+  @animals = Animal.all()
+erb(:update_staff)
+end
+
 #Staff save updates
-post '/vet-surgery/staff/:id/edit' do
+post '/vet-surgery/staff/:id' do
   staff = Staff.new(params)
   staff.update
-  redirect to "/vet-surgery/staff/#{params['id']}"
+  redirect to "/vet-surgery/staff/#{params[:id]}"
 end
 
 #Delete Staff
