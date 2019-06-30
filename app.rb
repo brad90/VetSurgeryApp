48ce4521ca474ee8631bf31ebfbe65ab37e2cd40
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-06-30 21:47:58 +0100

    update

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ require_relative('controllers/vet_surgery_staff_controller.rb')
 require_relative('controllers/vet_surgery_visits_controller.rb')
 
 get'/' do
-  erb( :'index/index' )
+  erb( :'/views/index/index' )
 end
