d20db25fa1c0f77bfb757d4b05e39c21a5d63cf4
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-06-30 21:40:41 +0100

    update to heroku

--- app.rb
+++ app.rb
@@ -3,6 +3,6 @@ require_relative('controllers/vet_surgery_animal_controller.rb')
 require_relative('controllers/vet_surgery_staff_controller.rb')
 require_relative('controllers/vet_surgery_visits_controller.rb')
 
-get '/' do
-  erb( :'animals/index' )
+get'/' do
+  erb( :'index/index' )
 end
