2511720c86440f7cc7ee6fd6f7790af064df6589
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-06-30 21:55:42 +0100

    update

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require( 'sinatra' )
 require_relative('controllers/vet_surgery_animal_controller.rb')
 require_relative('controllers/vet_surgery_staff_controller.rb')
 require_relative('controllers/vet_surgery_visits_controller.rb')
+require('pg')
 
 get'/' do
   erb( :'/views/index/index' )
